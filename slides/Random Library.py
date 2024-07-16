fd39127008b963946fcef3591be43f5b2e39b85b
JustNunuz <nunudzaimrewa222@gmail.com>
2024-07-16 12:16:33 +0200

    new files

---
+++ slides/Random Library.py
@@ -0,0 +1,5 @@
+#import a function from the random library
+import random 
+
+print(random.randint(1,10))
+#Display a random number between 1 and 10 inclusive
---
+++ slides/Random choice from list.py
@@ -0,0 +1,6 @@
+import random
+
+choice= [1,2,3,4,5,6]
+#List can have numbers or words
+
+print(random.choice(choice))

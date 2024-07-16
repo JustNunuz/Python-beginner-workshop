76e0622a7defd810db3e89403f1e4306ee1a420f
JustNunuz <nunudzaimrewa222@gmail.com>
2024-07-16 14:51:32 +0200

    if files

---
+++ workshop/If statement1.py
@@ -0,0 +1,7 @@
+name=input("What is your name:")
+
+if name==  "Nunu":
+    print("Hi Nunu")
+    
+else:
+    print("Hi "+ name)
---
+++ workshop/If statement2.py
@@ -0,0 +1,7 @@
+number= int(input("Enter a number:"))
+
+if  number >10:
+    print("Number greater than 10")
+    
+else:
+    print("Number less than 10")
